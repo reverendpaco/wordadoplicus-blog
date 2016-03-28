0eebd688d0ebd14f6d22f8dae0db92a2f126e03c
Daniel Eklund <doeklund@gmail.com>
2016-03-28 17:33:43 -0400

    getting everything to run from a vagrant instance

---
+++ install-basics.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+#apt-get install -y apache2
+#rm -rf /var/www
+#ln -fs /vagrant /var/www
+apt-get install -y vim
+apt-get install -y tree
+apt-get install -y tmux
+apt-get install -y git
+apt-get install -y grc
---
+++ install-tools.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/opt/packages/hugo && cd $_
+wget https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz
+gzip -dc hugo_0.15_linux_amd64.tar.gz | tar xf -
+rm hugo_0.15_linux_amd64.tar.gz
+
+mkdir ~/bin
+ln -s ~/opt/packages/hugo/hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 ~/bin/hugo
+
+ln -s /vagrant /home/vagrant/site
+
+cd /home/vagrant
+ln -s /vagrant/.tmux.conf
+ln -s /vagrant/.tmux-session
+ln -s /vagrant/tmux-session.sh
+tmux-session.sh restore
+
+curl -L https://github.com/reverendpaco/vimrc/archive/master.tar.gz | tar xzv && mv vimrc-master .vim
+
